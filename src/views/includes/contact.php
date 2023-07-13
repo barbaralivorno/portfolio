6d2cb3573845f889fca759661af826a2aaeea5f3
Barbara <livornobarbara@gmail.com>
2023-07-13 11:26:32 +0200

    contact

--- src/views/includes/contact.php
+++ src/views/includes/contact.php
@@ -1,3 +1,19 @@
 <section class="home__contact contact">
-    <h2 class="home__contact-title contact__title title title--white">Let's Talk</h2>
+  <h2 class="home__contact-title contact__title title title--white">Let's Talk</h2>
+  <div class="home__contact-container contact__container">
+    <div class="home__contact-data contact__data">
+      <h3 class="home__contact-data-title contact__data-title">Email</h3>
+      <a class="home__contact-data-link contact__data-link" href="mailto:livornobarbara@gmail.com"><span
+          class="home__contact-data-link-icon contact__data-link-icon"><?= file_get_contents(asset_path('images/arrow-down.svg')) ?></span>livornobarbara@gmail.com</a>
+    </div>
+    <div class="home__contact-data contact__data">
+      <h3 class="home__contact-data-title contact__data-title">Social Netoworks</h3>
+      <div class="home__contact-data-group contact__data-group">
+        <a class="home__contact-data-link contact__data-link" href="https://www.linkedin.com/in/barbaralivorno/"><span
+            class="home__contact-data-link-icon contact__data-link-icon"><?= file_get_contents(asset_path('images/arrow-down.svg')) ?></span>linkedin</a>
+        <a class="home__contact-data-link contact__data-link" href="https://github.com/barbaralivorno"><span
+            class="home__contact-data-link-icon contact__data-link-icon"><?= file_get_contents(asset_path('images/arrow-down.svg')) ?></span>github</a>
+      </div>
+    </div>
+  </div>
 </section>
