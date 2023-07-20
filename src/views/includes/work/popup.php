4ddb5a8c33114cab42248f3dcd95151b724fce6c
Barbara <livornobarbara@gmail.com>
2023-07-20 10:17:02 +0200

    popup

---
+++ src/views/includes/work/popup.php
@@ -0,0 +1,5 @@
+<div class="work__popup popup">
+  <div class="popup__body">
+    <button class="popup__body-close"><?= file_get_contents(asset_path('images/icon-close.svg')) ?></button>
+  </div>
+</div>
