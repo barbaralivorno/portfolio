a35ad11093c2cbb0c15e1207ccd639d8ff1a3fcd
Barbara <livornobarbara@gmail.com>
2023-07-28 16:38:40 +0200

    Change scroll button text in hero

--- src/views/includes/hero.php
+++ src/views/includes/hero.php
@@ -1,8 +1,7 @@
 <section class="hero" id="hero">
   <h2 class="hero__text">
   </h2>
-  <a class="hero__button scroll-button" href="#about"><span class="hero__button-text scroll-button__text">See
-      More About
-      Me</span><span
+  <a class="hero__button scroll-button" href="#about"><span class="hero__button-text scroll-button__text">Know me
+      better</span><span
       class="hero__button-icon scroll-button__icon"><?= file_get_contents(asset_path('images/arrow-down.svg')) ?></span></a>
 </section>
