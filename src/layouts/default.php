c2bc12093f807b9c849ef22a8b83481bba79889d
Barbara <livornobarbara@gmail.com>
2023-07-31 22:24:05 +0200

    Favicon fixes

--- src/layouts/default.php
+++ src/layouts/default.php
@@ -15,8 +15,13 @@ $template_config = get_field('theme');
   <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
   <meta content="no-cache" http-equiv="Pragma" />
   <meta content="width=device-width, initial-scale=1.0, user-scalable=no" name="viewport" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
   <title><?= $page_title ?></title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
